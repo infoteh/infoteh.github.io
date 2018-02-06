2a686af53e9a24f25cd6960d410bb16f90e84a03
infoteh <36202194+infoteh@users.noreply.github.com>
2018-02-06 19:54:27 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $(".abs-menu").slideToggle('0');
+
+    $(".button").click(function(){
+
+
+                    $(".block").slideToggle('slow');
+
+
+    });
+
+});
